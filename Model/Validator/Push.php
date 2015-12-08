6fdace805f3655d3b2dffd0fa5701345a9b9aa60
vagrant <vagrant@mage2.dev>
2015-12-08 16:15:25 +0000

    added the first methods for the push validation and handlers

--- Model/Validator/Push.php
+++ Model/Validator/Push.php
@@ -40,16 +40,102 @@
 
 namespace TIG\Buckaroo\Model\Validator;
 
+/**
+ * Class Push
+ *
+ * @package TIG\Buckaroo\Model\Validator
+ */
 class Push implements \TIG\Buckaroo\Model\ValidatorInterface
 {
 
+    const BUCKAROO_SUCCESS           = 'BUCKAROO_SUCCESS';
+    const BUCKAROO_FAILED            = 'BUCKAROO_FAILED';
+    const BUCKAROO_ERROR             = 'BUCKAROO_ERROR';
+    const BUCKAROO_NEUTRAL           = 'BUCKAROO_NEUTRAL';
+    const BUCKAROO_PENDING_PAYMENT   = 'BUCKAROO_PENDING_PAYMENT';
+    const BUCKAROO_INCORRECT_PAYMENT = 'BUCKAROO_INCORRECT_PAYMENT';
+    const BUCKAROO_REJECTED          = 'BUCKAROO_REJECTED';
+
     /**
-     * @param array|object $data
-     *
-     * @return boolean
+     *  List of possible response codes sent by buckaroo.
+     *  This is the list for the BPE 3.0 gateway.
+     *  @var array $bpeResponseCodes
      */
-    public function validate($data)
+    public $bpeResponseCodes = array(
+        190 => array(
+            'message' => 'Success',
+            'status'  => self::BUCKAROO_SUCCESS,
+        ),
+        490 => array(
+            'message' => 'Payment failure',
+            'status'  => self::BUCKAROO_FAILED,
+        ),
+        491 => array(
+            'message' => 'Validation error',
+            'status'  => self::BUCKAROO_FAILED,
+        ),
+        492 => array(
+            'message' => 'Technical error',
+            'status'  => self::BUCKAROO_ERROR,
+        ),
+        690 => array(
+            'message' => 'Payment rejected',
+            'status'  => self::BUCKAROO_REJECTED,
+        ),
+        790 => array(
+            'message' => 'Waiting for user input',
+            'status'  => self::BUCKAROO_PENDING_PAYMENT,
+        ),
+        791 => array(
+            'message' => 'Waiting for processor',
+            'status'  => self::BUCKAROO_PENDING_PAYMENT,
+        ),
+        792 => array(
+            'message' => 'Waiting on consumer action',
+            'status'  => self::BUCKAROO_PENDING_PAYMENT,
+        ),
+        793 => array(
+            'message' => 'Payment on hold',
+            'status'  => self::BUCKAROO_PENDING_PAYMENT,
+        ),
+        890 => array(
+            'message' => 'Cancelled by consumer',
+            'status'  => self::BUCKAROO_FAILED,
+        ),
+        891 => array(
+            'message' => 'Cancelled by merchant',
+            'status'  => self::BUCKAROO_FAILED,
+        ),
+    );
+
+    /**
+     * Checks if the status code is returned by the bpe push and is valid.
+     * @param $code
+     *
+     * @return Array
+     */
+    public function validateStatusCode($code)
+    {
+        if ( isset($this->bpeResponseCodes[$code]) ) {
+            return $this->bpeResponseCodes[$code];
+        }else{
+            return array(
+                'message' => 'Onbekende responsecode: ' . $code,
+                'status'  => self::BUCKAROO_NEUTRAL,
+                'code'    => $code,
+            );
+        }
+    }
+
+    /**
+     * @param $signature
+     *
+     * @todo build the validator that determines the signature using array sorting and the SHA1 hash algorithm
+     * @return bool
+     */
+    public function validateSignature($signature)
     {
         return true;
     }
+
 }
