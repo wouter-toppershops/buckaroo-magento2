083e0e5025f8cdf0e675a1bd945cb94c050e1d90
vagrant <vagrant@mage2.dev>
2015-12-09 12:43:18 +0000

    added message for sepadirectdebit orders

--- Setup/InstallData.php
+++ Setup/InstallData.php
@@ -55,24 +55,6 @@ class InstallData implements \Magento\Framework\Setup\InstallDataInterface
      */
     public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
-        /**
-         * Install order statuses from config
-         */
-        $setup->getConnection()->insert(
-            $setup->getTable('sales_order_status'),
-            [
-                'status' => 'tig_buckaroo_pending_payment',
-                'label'  => __('TIG Buckaroo Pending Payment'),
-            ]
-        );
-
-        $setup->getConnection()->insert(
-            $setup->getTable('sales_order_status_state'),
-            [
-                'status'     => 'tig_buckaroo_pending_payment',
-                'state'      => 'processing',
-                'is_default' =>  0,
-            ]
-        );
+        // Placeholder for future install scripts.
     }
 }
