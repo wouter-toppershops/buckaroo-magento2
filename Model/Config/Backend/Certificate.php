68db6ed037d78b6c86582e0c55d0f00a95d17987
Mickey Beijer <mickey.beijer@tig.nl>
2015-12-15 16:03:46 +0000

    added basic certificate upload options

---
+++ Model/Config/Backend/Certificate.php
@@ -0,0 +1,144 @@
+<?php
+/**
+ *                  ___________       __            __
+ *                  \__    ___/____ _/  |_ _____   |  |
+ *                    |    |  /  _ \\   __\\__  \  |  |
+ *                    |    | |  |_| ||  |   / __ \_|  |__
+ *                    |____|  \____/ |__|  (____  /|____/
+ *                                              \/
+ *          ___          __                                   __
+ *         |   |  ____ _/  |_   ____ _______   ____    ____ _/  |_
+ *         |   | /    \\   __\_/ __ \\_  __ \ /    \ _/ __ \\   __\
+ *         |   ||   |  \|  |  \  ___/ |  | \/|   |  \\  ___/ |  |
+ *         |___||___|  /|__|   \_____>|__|   |___|  / \_____>|__|
+ *                  \/                           \/
+ *                  ________
+ *                 /  _____/_______   ____   __ __ ______
+ *                /   \  ___\_  __ \ /  _ \ |  |  \\____ \
+ *                \    \_\  \|  | \/|  |_| ||  |  /|  |_| |
+ *                 \______  /|__|    \____/ |____/ |   __/
+ *                        \/                       |__|
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Creative Commons License.
+ * It is available through the world-wide-web at this URL:
+ * http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ * If you are unable to obtain it through the world-wide-web, please send an email
+ * to servicedesk@totalinternetgroup.nl so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future. If you wish to customize this module for your
+ * needs please contact servicedesk@totalinternetgroup.nl for more information.
+ *
+ * @copyright   Copyright (c) 2015 Total Internet Group B.V. (http://www.totalinternetgroup.nl)
+ * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ */
+namespace TIG\Buckaroo\Model\Config\Backend;
+
+class Certificate extends \Magento\Framework\App\Config\Value //\Magento\Config\Model\Config\Backend\File
+{
+    /**
+     * @var \Magento\Framework\ObjectManagerInterface
+     */
+    protected $objectManager;
+
+    /**
+     * Test
+     * @param \Magento\Framework\ObjectManagerInterface $objectmanager
+     */
+    public function __construct(
+        \Magento\Framework\ObjectManagerInterface $objectmanager,
+        \Magento\Framework\Model\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Framework\App\Config\ScopeConfigInterface $config,
+        \Magento\Framework\App\Cache\TypeListInterface $cacheTypeList,
+        \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
+        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
+        array $data = []
+    ) {
+        $this->objectManager = $objectmanager;
+
+        parent::__construct($context, $registry, $config, $cacheTypeList, $resource, $resourceCollection);
+    }
+
+    public function save()
+    {
+        //type == application/x-x509-ca-cert
+        if (!empty($this->getFieldsetDataValue('certificate_upload')['name'])) {
+            $certFile = $this->getFieldsetDataValue('certificate_upload');
+
+            if (!$this->validExtension($certFile['name'])) {
+                throw new \Exception('Disallowed file type.');
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Check if extension is valid
+     *
+     * @param String $filename Name of uplpaded file
+     *
+     * @return bool
+     */
+    protected function validExtension($filename)
+    {
+        $allowedExtensions = ['pem'];
+
+        $extension = pathinfo($filename, PATHINFO_EXTENSION);
+
+        return in_array(strtolower($extension), $allowedExtensions);
+    }
+
+    /*const UPLOAD_DIR = 'buckcert';
+
+    protected function _getUploadDir()
+    {
+        return $this->_mediaDirectory->getAbsolutePath(
+            $this->_appendScopeInfo(
+                self::UPLOAD_DIR
+            )
+        );
+    }
+
+    protected function _addWhetherScopeInfo()
+    {
+        return true;
+    }
+
+    protected function _getAllowedExtensions()
+    {
+        return ['pem'];
+    }*/
+
+    /*
+    protected $_filesystem;
+
+    protected $_fileUploaderFactory;
+
+    protected $_coreRegistry;
+
+    protected $_objectManager;
+
+    public function __construct(
+        \Magento\Framework\Filesystem $filesystem,
+        \Magento\MediaStorage\Model\File\UploaderFactory $fileUploaderFactory,
+        \Magento\Framework\Registry $coreRegistry,
+        \Magento\Framework\ObjectManagerInterface $objectManager
+    ) {
+        $this->_filesystem          = $filesystem;
+        $this->_fileUploaderFactory = $fileUploaderFactory;
+        $this->_coreRegistry        = $coreRegistry;
+        $this->_objectManager       = $objectManager;
+    }
+
+    public function afterSave($object)
+    {
+        //Save crap todo
+    }
+    */
+}
