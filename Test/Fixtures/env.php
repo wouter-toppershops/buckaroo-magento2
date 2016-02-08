c696fc20b614a42574c8ac347b2ecc73762bdc1b
Michiel Gerritsen <michiel.gerritsen@tig.nl>
2016-02-08 12:51:00 +0100

    Added and modified files to make it work with Selenium

---
+++ Test/Fixtures/env.php
@@ -0,0 +1,43 @@
+<?php
+return array (
+  'backend' =>
+  array (
+    'frontName' => 'admin',
+  ),
+  'install' =>
+  array (
+    'date' => 'Mon, 01 Feb 2016 15:21:04 +0000',
+  ),
+  'crypt' =>
+  array (
+    'key' => 'ef0e78a6c44764690780138ce89b7791',
+  ),
+  'session' =>
+  array (
+    'save' => 'db',
+  ),
+  'db' =>
+  array (
+    'table_prefix' => '',
+    'connection' =>
+    array (
+      'default' =>
+      array (
+        'host' => 'localhost',
+        'dbname' => 'buckaroo',
+        'username' => 'buckaroo',
+        'password' => '12345',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' =>
+  array (
+    'default_setup' =>
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'default',
+);
