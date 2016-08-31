77c9e4703cbdffcf5e68f59ac85a911eeba2d942
Mickey Beijer <mickey.beijer@tig.nl>
2016-08-31 16:21:49 +0200

    Added congifuration page to manage available giftcards

---
+++ Controller/Adminhtml/Giftcard/Index.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ *                  ___________       __            __
+ *                  \__    ___/____ _/  |_ _____   |  |
+ *                    |    |  /  _ \\   __\\__  \  |  |
+ *                    |    | |  |_| ||  |   / __ \_|  |__
+ *                    |____|  \____/ |__|  (____  /|____/
+ *                                              \/
+ *          ___          __                                   __
+ *         |   |  ____ _/  |_   ____ _______   ____    ____ _/  |_
+ *         |   | /    \\   __\_/ __ \\_  __ \ /    \ _/ __ \\   __\
+ *         |   ||   |  \|  |  \  ___/ |  | \/|   |  \\  ___/ |  |
+ *         |___||___|  /|__|   \_____>|__|   |___|  / \_____>|__|
+ *                  \/                           \/
+ *                  ________
+ *                 /  _____/_______   ____   __ __ ______
+ *                /   \  ___\_  __ \ /  _ \ |  |  \\____ \
+ *                \    \_\  \|  | \/|  |_| ||  |  /|  |_| |
+ *                 \______  /|__|    \____/ |____/ |   __/
+ *                        \/                       |__|
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Creative Commons License.
+ * It is available through the world-wide-web at this URL:
+ * http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ * If you are unable to obtain it through the world-wide-web, please send an email
+ * to servicedesk@totalinternetgroup.nl so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future. If you wish to customize this module for your
+ * needs please contact servicedesk@totalinternetgroup.nl for more information.
+ *
+ * @copyright   Copyright (c) 2016 Total Internet Group B.V. (http://www.totalinternetgroup.nl)
+ * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ */
+namespace TIG\Buckaroo\Controller\Adminhtml\Giftcard;
+
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+use TIG\Buckaroo\Model\ResourceModel\Giftcard\Collection;
+
+class Index extends \Magento\Backend\App\Action
+{
+    /** @var  PageFactory */
+    protected $resultPageFactory;
+
+    /**
+     * @var Collection
+     */
+    protected $giftcardCollection;
+
+    /**
+     * @param Context              $context
+     * @param PageFactory          $resultPageFactory
+     * @param Collection $giftcardCollection
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory,
+        Collection $giftcardCollection
+    ) {
+        parent::__construct($context);
+
+        $this->resultPageFactory = $resultPageFactory;
+        $this->giftcardCollection = $giftcardCollection;
+    }
+
+    /**
+     * @return \Magento\Backend\Model\View\Result\Page
+     */
+    public function execute()
+    {
+        /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('TIG_Buckaroo::buckaroo_giftcards');
+        $resultPage->getConfig()->getTitle()->prepend(__('Buckaroo Giftcards'));
+
+        return $resultPage;
+    }
+
+    /**
+     * Is the user allowed to view the blog post grid.
+     *
+     * @return bool
+     */
+    protected function _isAllowed()
+    {
+        return $this->_authorization->isAllowed('TIG_Buckaroo::buckaroo_giftcards');
+    }
+}
