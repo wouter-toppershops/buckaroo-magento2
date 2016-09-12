79557a6f004cf94cf927af0878d600a8b3a5fae8
Mickey Beijer <mickey.beijer@tig.nl>
2016-09-12 11:15:05 +0200

    added another unittest for Giftcard model

---
+++ Test/Unit/Model/GiftcardTest.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ *                  ___________       __            __
+ *                  \__    ___/____ _/  |_ _____   |  |
+ *                    |    |  /  _ \\   __\\__  \  |  |
+ *                    |    | |  |_| ||  |   / __ \_|  |__
+ *                    |____|  \____/ |__|  (____  /|____/
+ *                                              \/
+ *          ___          __                                   __
+ *         |   |  ____ _/  |_   ____ _______   ____    ____ _/  |_
+ *         |   | /    \\   __\_/ __ \\_  __ \ /    \ _/ __ \\   __\
+ *         |   ||   |  \|  |  \  ___/ |  | \/|   |  \\  ___/ |  |
+ *         |___||___|  /|__|   \_____>|__|   |___|  / \_____>|__|
+ *                  \/                           \/
+ *                  ________
+ *                 /  _____/_______   ____   __ __ ______
+ *                /   \  ___\_  __ \ /  _ \ |  |  \\____ \
+ *                \    \_\  \|  | \/|  |_| ||  |  /|  |_| |
+ *                 \______  /|__|    \____/ |____/ |   __/
+ *                        \/                       |__|
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Creative Commons License.
+ * It is available through the world-wide-web at this URL:
+ * http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ * If you are unable to obtain it through the world-wide-web, please send an email
+ * to servicedesk@totalinternetgroup.nl so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future. If you wish to customize this module for your
+ * needs please contact servicedesk@totalinternetgroup.nl for more information.
+ *
+ * @copyright   Copyright (c) 2016 Total Internet Group B.V. (http://www.totalinternetgroup.nl)
+ * @license     http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ */
+namespace TIG\Buckaroo\Test\Unit\Model;
+
+use TIG\Buckaroo\Model\Giftcard;
+
+class GiftcardTest extends \TIG\Buckaroo\Test\BaseTest
+{
+    /**
+    * @var Giftcard
+    */
+    protected $object;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->object = $this->objectManagerHelper->getObject(Giftcard::class);
+    }
+
+    /**
+     * @return array
+     */
+    public function servicecodeProvider()
+    {
+        return array(
+            array(
+                'servicecode' => 'shopgiftcard',
+                'expected' => 'shopgiftcard'
+            ),
+            array(
+                'servicecode' => 'bookgiftcard',
+                'expected' => 'bookgiftcard'
+            ),
+            array(
+                'servicecode' => 'discountcard',
+                'expected' => 'discountcard'
+            )
+        );
+    }
+
+    /**
+     * @param $servicecode
+     * @param $expected
+     *
+     * @dataProvider servicecodeProvider
+     */
+    public function testShouldBeAbleToSetAndGetServicecode($servicecode, $expected)
+    {
+        $object = $this->object;
+        $object->setServicecode($servicecode);
+
+        $this->assertEquals($expected, $object->getServicecode());
+    }
+
+    /**
+     * @return array
+     */
+    public function labelProvider()
+    {
+        return array(
+            array(
+                'label' => 'Webshop Giftcard',
+                'expected' => 'Webshop Giftcard'
+            ),
+            array(
+                'label' => 'Book Giftcard',
+                'expected' => 'Book Giftcard'
+            ),
+            array(
+                'label' => 'Discount Card',
+                'expected' => 'Discount Card'
+            )
+        );
+    }
+
+    /**
+     * @param $label
+     * @param $expected
+     *
+     * @dataProvider labelProvider
+     */
+    public function testShouldBeAbleToSetAndGetLabel($label, $expected)
+    {
+        $object = $this->object;
+        $object->setLabel($label);
+
+        $this->assertEquals($expected, $object->getLabel());
+    }
+}
