8ae51929b154eccd4e141f0cbe5a79c32b11a018
Robert Grundeken <robert.grundeken@tig.nl>
2017-03-23 13:52:15 +0100

    Push matches order now on brq_ordernumber instead of invoicenumber

--- Model/Push.php
+++ Model/Push.php
@@ -196,10 +196,10 @@ class Push implements PushInterface
 
         //Check if the order can receive further status updates
         $this->order = $this->objectManager->create(Order::class)
-            ->loadByIncrementId($this->postData['brq_invoicenumber']);
+            ->loadByIncrementId($this->postData['brq_ordernumber']);
 
         if (!$this->order->getId()) {
-            $this->debugger->addToMessage('Order could not be loaded by brq_invoicenumber');
+            $this->debugger->addToMessage('Order could not be loaded by brq_ordernumber');
             // try to get order by transaction id on payment.
             $this->order = $this->getOrderByTransactionKey($this->postData['brq_transactions']);
         }
