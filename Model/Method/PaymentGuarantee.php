8c1b0e4097fc738dc624cb60b8208d881ac22011
Mickey Beijer <mickey.beijer@tig.nl>
2017-04-13 16:35:22 +0200

    Dob field in checkout can now be filled out as dd/mm/yyyy

--- Model/Method/PaymentGuarantee.php
+++ Model/Method/PaymentGuarantee.php
@@ -235,8 +235,10 @@ class PaymentGuarantee extends AbstractMethod
             'customer_billingName',
             $additionalData['customer_billingName']
         );
-        $this->getInfoInstance()->setAdditionalInformation('customer_DoB', $additionalData['customer_DoB']);
         $this->getInfoInstance()->setAdditionalInformation('customer_iban', $additionalData['customer_iban']);
+
+        $dobDate = \DateTime::createFromFormat('d/m/Y', $additionalData['customer_DoB']);
+        $this->getInfoInstance()->setAdditionalInformation('customer_DoB', $dobDate->format('Y-m-d'));
 
         return $this;
     }
