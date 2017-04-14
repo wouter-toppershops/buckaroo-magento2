b472109d7f4b22bf1a09a28d31c5ca4e53202641
Mickey Beijer <mickey.beijer@tig.nl>
2017-04-14 11:43:28 +0200

    Proccesed feedback of #16 : Applied changes to Afterpay

--- Model/Method/Afterpay2.php
+++ Model/Method/Afterpay2.php
@@ -152,8 +152,12 @@ class Afterpay2 extends AbstractMethod
                 'customer_billingName',
                 $additionalData['customer_billingName']
             );
-            $this->getInfoInstance()->setAdditionalInformation('customer_DoB', $additionalData['customer_DoB']);
             $this->getInfoInstance()->setAdditionalInformation('customer_iban', $additionalData['customer_iban']);
+
+            $dobDate = \DateTime::createFromFormat('d/m/Y', $additionalData['customer_DoB']);
+            $dobDate = (!$dobDate ? $additionalData['customer_DoB'] : $dobDate->format('Y-m-d'));
+            $this->getInfoInstance()->setAdditionalInformation('customer_DoB', $dobDate);
+
             if (isset($additionalData['selectedBusiness'])
                 && $additionalData['selectedBusiness'] == self::BUSINESS_METHOD_B2B
             ) {
