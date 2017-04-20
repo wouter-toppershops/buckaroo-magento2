e67adddca579126a5ec7e37321b8446c40f6639e
Mickey Beijer <mickey.beijer@tig.nl>
2017-04-20 18:39:20 +0200

    Refactored code in Model\Config, added interfaces and repository for Certificate Model

--- Model/Config/Backend/CertificateLabel.php
+++ Model/Config/Backend/CertificateLabel.php
@@ -41,36 +41,6 @@ namespace TIG\Buckaroo\Model\Config\Backend;
 class CertificateLabel extends \Magento\Framework\App\Config\Value
 {
     /**
-     * @var \Magento\Framework\ObjectManagerInterface
-     */
-    protected $objectManager;
-
-    /**
-     * @param \Magento\Framework\ObjectManagerInterface               $objectmanager
-     * @param \Magento\Framework\Model\Context                        $context
-     * @param \Magento\Framework\Registry                             $registry
-     * @param \Magento\Framework\App\Config\ScopeConfigInterface      $config
-     * @param \Magento\Framework\App\Cache\TypeListInterface          $cacheTypeList
-     * @param \Magento\Framework\Model\ResourceModel\AbstractResource $resource
-     * @param \Magento\Framework\Data\Collection\AbstractDb           $resourceCollection
-     * @param array                                                   $data
-     */
-    public function __construct(
-        \Magento\Framework\ObjectManagerInterface $objectmanager,
-        \Magento\Framework\Model\Context $context,
-        \Magento\Framework\Registry $registry,
-        \Magento\Framework\App\Config\ScopeConfigInterface $config,
-        \Magento\Framework\App\Cache\TypeListInterface $cacheTypeList,
-        \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
-        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
-        array $data = []
-    ) {
-        $this->objectManager = $objectmanager;
-
-        parent::__construct($context, $registry, $config, $cacheTypeList, $resource, $resourceCollection);
-    }
-
-    /**
      * Prevent saving the value by returning from the function immediately
      *
      * We don't need to safe certificate_label since it's handled in the certificate backend model.
