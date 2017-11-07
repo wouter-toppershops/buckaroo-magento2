9e9b977bb6eba240f690300837a8bb0696e44cee
Mickey Beijer <mickey.beijer@tig.nl>
2017-11-07 16:20:22 +0100

    BUCKM2-112 : Fixed broken unittests

--- Test/Unit/Model/Refund/PushTest.php
+++ Test/Unit/Model/Refund/PushTest.php
@@ -239,8 +239,9 @@ class PushTest extends \TIG\Buckaroo\Test\BaseTest
         $this->order->shouldReceive('getBaseToOrderRate')->once()->andReturn(1);
         $this->order->shouldReceive('getBaseTotalRefunded')->once()->andReturn(null);
         $this->order->shouldReceive('getBaseBuckarooFeeInvoiced')->andReturn(10);
+        $this->order->shouldReceive('getBuckarooFeeBaseTaxAmountInvoiced')->andReturn(5);
 
-        $this->assertEquals(90, $this->object->getAdjustmentRefundData());
+        $this->assertEquals(85, $this->object->getAdjustmentRefundData());
     }
 
     /**
